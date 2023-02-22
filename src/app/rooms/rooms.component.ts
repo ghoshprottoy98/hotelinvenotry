c39907400a83ee57fb56e4ddbef2104a7b6b7680
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-22 11:32:16 +0600

    master

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {OnInit} from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Room } from "./rooms"; 
+import { Roomlist } from "./rooms"; 
 
 @Component({
   selector: 'hotelinv-rooms',
@@ -14,6 +16,48 @@ export class RoomsComponent implements OnInit {
   numberOfRooms =10;
 
   hideRooms= false;
+
+  rooms: Room  = {
+
+   totalRooms: 15,
+    availableRooms: 10,
+    bookedRooms: 5
+  }
+
+  roomlist: Roomlist [] = [
+    {
+    roomNumber:1,
+    roomType: 'Deluxe Room',
+    amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+    price: 500,
+    photos: 'https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    checkinTime: new Date ('11-Feb-2023'),
+    checkoutTime: new Date ('12-Feb-2023'),
+  },
+
+  {
+  roomNumber:2,
+  roomType: 'Deluxe Room',
+  amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+  price: 1000,
+  photos: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  checkinTime: new Date ('11-Feb-2023'),
+  checkoutTime: new Date ('12-Feb-2023'),
+},
+
+{
+  roomNumber:3,
+  roomType: 'Private Suite',
+  amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+  price: 1500,
+  photos: 'https://images.unsplash.com/photo-1585412727339-54e4bae3bbf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  checkinTime: new Date ('11-Feb-2023'),
+  checkoutTime: new Date ('12-Feb-2023'),
+},
+
+];
+
+
 
   constuctor(): void {}
 
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,19 @@
+export interface Room {
+
+    totalRooms: number;
+    availableRooms: number;
+    bookedRooms:number;
+
+}
+
+export interface Roomlist{
+
+    roomNumber: number;
+    roomType: string;
+    amenities: string;
+    price: number;
+    photos: string;
+    checkinTime: Date;
+    checkoutTime: Date;
+    
+}
