6dc2fee7e1ccb4e83596d0d4f7982ec36328fa44
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-22 13:50:43 +0600

    master

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Roomlist } from '../rooms';
+
+@Component({
+  selector: 'hotelinv-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.css']
+})
+
+export class RoomsListComponent implements OnInit{
+
+
+  @Input() rooms: Roomlist[] = [];
+
+  @Input() title:string = ' ';
+
+
+  @Output() selectedRoom= new EventEmitter<Roomlist>();
+
+
+  constuctor(): void {}
+
+  ngOnInit(): void{}
+
+  selectRoom(room: Roomlist) {
+    this.selectedRoom.emit(room);
+  }
+
+
+}
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -6,7 +6,7 @@ import { Roomlist } from "./rooms";
 @Component({
   selector: 'hotelinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  styleUrls: ['./rooms.component.css'],
 })
 
 export class RoomsComponent implements OnInit {
@@ -17,6 +17,8 @@ export class RoomsComponent implements OnInit {
 
   hideRooms= false;
 
+  selectedRoom!: Roomlist;
+
   rooms: Room  = {
 
    totalRooms: 15,
@@ -24,50 +26,70 @@ export class RoomsComponent implements OnInit {
     bookedRooms: 5
   }
 
-  roomlist: Roomlist [] = [];
+  roomlist: Roomlist [] = [
+    {
+    roomNumber:1,
+    roomType: 'Deluxe Room',
+    amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+    price: 500,
+    photos: 'https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    checkinTime: new Date ('11-Feb-2023'),
+    checkoutTime: new Date ('12-Feb-2023'),
+    rating: 4.5,
+  },
+
+  {
+  roomNumber:2,
+  roomType: 'Deluxe Room',
+  amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+  price: 1000,
+  photos: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  checkinTime: new Date ('11-Feb-2023'),
+  checkoutTime: new Date ('12-Feb-2023'),
+  rating: 4.,
+},
+
+{
+  roomNumber:3,
+  roomType: 'Private Suite',
+  amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+  price: 1500,
+  photos: 'https://images.unsplash.com/photo-1585412727339-54e4bae3bbf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  checkinTime: new Date ('11-Feb-2023'),
+  checkoutTime: new Date ('12-Feb-2023'),
+  rating: 3.5,
+},
+
+]
 
   constuctor(): void {}
 
-  ngOnInit(): void{
+  ngOnInit(): void{}
 
-    this.roomlist = [
-    {
-      roomNumber:1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-      price: 500,
-      photos: 'https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
-      checkinTime: new Date ('11-Feb-2023'),
-      checkoutTime: new Date ('12-Feb-2023'),
-      rating: 4.5,
-    },
-  
-    {
-    roomNumber:2,
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room: Roomlist)
+  {
+  console.log(room);
+  }
+
+
+  addRoom()
+  { 
+    const room: Roomlist = {
+    roomNumber: 4,
     roomType: 'Deluxe Room',
     amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
     price: 1000,
     photos: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
     checkinTime: new Date ('11-Feb-2023'),
     checkoutTime: new Date ('12-Feb-2023'),
-    rating: 4.,
-  },
-  
-  {
-    roomNumber:3,
-    roomType: 'Private Suite',
-    amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
-    price: 1500,
-    photos: 'https://images.unsplash.com/photo-1585412727339-54e4bae3bbf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-    checkinTime: new Date ('11-Feb-2023'),
-    checkoutTime: new Date ('12-Feb-2023'),
-    rating: 3.5,
-  },
-  
-   ];
-  }
+    rating: 2,
 
-  toggle(): void {
-    this.hideRooms = !this.hideRooms;
-  }
+  };
+
+  this.roomlist.push(room);
+}
 }
